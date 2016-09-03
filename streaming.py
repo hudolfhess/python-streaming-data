4ac36feaece813fb91638e4017bd9013f80ac83b
Hudolf Hess <hudolf@gmail.com>
2016-09-03 13:50:02 -0300

    websocket server and client example

---
+++ streaming.py
@@ -0,0 +1,55 @@
+import json
+import socket
+import MySQLdb
+from time import sleep
+from threading import Thread
+
+
+HOST = ''
+PORT = 8080
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+
+
+def dictfetchall(cursor):
+    desc = cursor.description
+    return [
+        dict(zip([col[0] for col in desc], row))
+        for row in cursor.fetchall()
+    ]
+
+
+def get_data(db, last_id=0):
+    c = db.cursor()
+    c.execute("""select * from vendas_empresa where id > %s order by id asc limit 50""" % last_id)
+    return dictfetchall(c)
+
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+
+
+def get_database_connection():
+    return MySQLdb.connect(host='localhost', user='root', db='representante')
+
+
+def listen(conn):
+    db = get_database_connection()
+    streaming_data = get_data(db)
+
+    while streaming_data:
+        for data in streaming_data:
+            # conn.sendall(json.dumps(data, default=date_handler))
+            sleep(1)
+        streaming_data = get_data(db, data['id'])
+
+
+while True:
+    conn, addr = s.accept()
+    print 'Connected by', addr
+
+    server_thread = Thread(target=listen, args=[conn])
+    server_thread.start()
+
+    conn.close()
